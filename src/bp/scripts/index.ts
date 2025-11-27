56a7169f99486f69f693fe480b216b1421ae1e22
LuckedCoronet <lc45@lc-studios.net>
2025-11-27 09:33:18 +0900

    chore(bp): add empty main script (index.ts)

---
+++ src/bp/scripts/index.ts
@@ -0,0 +1,6 @@
+// This is the entry point to all behavior pack scripts.
+
+// I recommend that you write your important logic in other scripts, and "activate" them by
+// importing them from here.
+//
+// import "./script-with-logic";
